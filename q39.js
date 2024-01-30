d177c6cee9ea75a7815b57a62a8945e43b0a7015
syeda aroosa <bcsm-f18-526@superior.edu.pk>
2024-01-30 23:36:24 +0500

    q39.ts

---
+++ q39.js
@@ -0,0 +1,12 @@
+"use strict";
+// City Names: Write a function called city_country() that takes in the name of a city and its country. The function should return a string formatted like this:
+// "Lahore, Pakistan"
+// Call your function with at least three city-country pairs, and print the value that’s returned.
+// Function to describe a city and its country
+function describe_city1(city, country = 'Unknown') {
+    console.log(`${city} is in ${country}.`);
+}
+// Call the function for three different cities
+describe_city1('Karachi', 'Pakistan');
+describe_city1('New York', 'USA');
+describe_city1('Tokyo'); // This will use the default country ("Unknown")
