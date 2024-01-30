ac5837d751449b4965fb3de6bdf713d6ea9add6a
syeda aroosa <bcsm-f18-526@superior.edu.pk>
2024-01-30 22:36:25 +0500

    q20

---
+++ q20.js
@@ -0,0 +1,18 @@
+"use strict";
+// Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+// Array of programming languages
+let programmingLanguages = [
+    'JavaScript',
+    'TypeScript',
+    'Python',
+    'Java',
+    'C#',
+    'C++',
+    'Ruby',
+    'Swift',
+];
+// Print the list of programming languages
+console.log('List of Programming Languages:');
+programmingLanguages.forEach((language, index) => {
+    console.log(`${index + 1}. ${language}`);
+});
