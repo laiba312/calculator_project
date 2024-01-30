51f5c86953d2e2ebba73bb5ba7544aca33c7157b
syeda aroosa <bcsm-f18-526@superior.edu.pk>
2024-01-30 23:32:36 +0500

    q37 done

---
+++ q37.js
@@ -0,0 +1,12 @@
+"use strict";
+// Large Shirts: Modify the make_shirt() function so that shirts are large by default with a message that reads I love TypeScript. Make a large shirt and a medium shirt with the default message, and a shirt of any size with a different message.
+// Modified function to make a shirt with defaults
+function make_shirt1(size = 'large', message = 'I love TypeScript') {
+    console.log(`A ${size.toUpperCase()} shirt will be printed with the message: "${message}"`);
+}
+// Create a large shirt with the default message
+make_shirt1();
+// Create a medium shirt with the default message
+make_shirt1('medium');
+// Create a small shirt with a different message
+make_shirt1('small', 'JavaScript is fun!');
