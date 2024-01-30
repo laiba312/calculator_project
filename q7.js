4f93df1f2f3e3156ec642d4ec5c48249ff8adfd7
syeda aroosa <bcsm-f18-526@superior.edu.pk>
2024-01-30 21:47:11 +0500

    q7 done

---
+++ q7.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+/*Number Eight: Write addition, subtraction, multiplication,
+ and division operations that each result in the number 8.
+ Be sure to enclose your operations in print
+statements to see the results.*/
+let add = 4 + 4;
+console.log('addition: ' + add);
+let subtraction = 16 - 8;
+console.log('sub: ' + subtraction);
+let multiplication = 4 * 2;
+console.log('multiplication: ' + multiplication);
+let division = 16 / 2;
+console.log('division: ' + division);
