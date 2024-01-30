37e2218cd040ad1e91084c6b318f457daf2f335c
syeda aroosa <bcsm-f18-526@superior.edu.pk>
2024-01-30 23:48:09 +0500

    q42 done

---
+++ q42.js
@@ -0,0 +1,26 @@
+"use strict";
+// Great Magicians: Start with a copy of your program from Exercise 39. Write a function called make_great() that modifies the array of magicians by adding the phrase the Great to each magician’s name. Call show_magicians() to see that the list has actually been modified.
+// Function to show magicians
+function show_magicians1(magicians) {
+    for (let magician of magicians) {
+        console.log(magician);
+    }
+}
+// Function to make magicians great
+function make_great(magicians) {
+    // Create a new array with modified names
+    let greatMagicians = [];
+    for (let magician of magicians) {
+        greatMagicians.push(`${magician} the Great`);
+    }
+    return greatMagicians;
+}
+// Array of magician's names
+let magicianNames1 = ['David ', 'laiba', 'fatima', 'meryam'];
+// Call make_great to modify the array
+let greatMagicians = make_great(magicianNames1);
+// Call show_magicians to see the modified list
+console.log('Original Magicians:');
+show_magicians1(magicianNames1);
+console.log('\nGreat Magicians:');
+show_magicians1(greatMagicians);
