94a80a53e473943deac1b31ecdbdc5ca776962bb
syeda aroosa <bcsm-f18-526@superior.edu.pk>
2024-01-30 21:57:10 +0500

    q10 done

---
+++ q10.js
@@ -0,0 +1,17 @@
+"use strict";
+/*Adding Comments: Choose two of the programs you’ve written,
+ and add at least one comment to each. If you don’t have anything
+  specific to write because your programs are too simple at this point,
+  just add your name and the current date at the top of each program file.
+  Then write one sentence describing what the program does.*/
+// Author: [Your Name]
+// Date: [Current Date]
+// This program performs different arithmetic operations to result in the number 8 and prints the outcomes.
+// Addition to get 8
+console.log('5 + 3 =', 5 + 3); // 5 plus 3 equals 8
+// Subtraction to get 8
+console.log('10 - 2 =', 10 - 2); // 10 minus 2 equals 8
+// Multiplication to get 8
+console.log('4 * 2 =', 4 * 2); // 4 times 2 equals 8
+// Division to get 8
+console.log('16 / 2 =', 16 / 2); // 16 divided by 2 equals 8
