df03e41a835281b9aca5cc5e39f5834613e6ee3b
syeda aroosa <bcsm-f18-526@superior.edu.pk>
2024-01-30 21:52:54 +0500

    q8 done

---
+++ q8.ts
@@ -0,0 +1,11 @@
+/*You should create four lines that look like this:
+
+console.log(5 + 3)
+
+Your output should simply be four lines with the number
+ 8 appearing once on each line.*/
+
+console.log('add: ' + (5 + 3));
+console.log('sub: ' + (16 - 8));
+console.log('multi: ' + 4 * 2);
+console.log('divi: ' + 16 / 2);
