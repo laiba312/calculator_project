5628a407f1bc1cfc6553e346ef62350b4a9a22c0
syeda aroosa <bcsm-f18-526@superior.edu.pk>
2024-01-30 21:58:30 +0500

    q11 done

---
+++ q11.ts
@@ -0,0 +1,11 @@
+import { format } from 'node:path/posix';
+
+/*Names: Store the names of a few of your friends in a array called names.
+ Print each person’s name by accessing each element in the list, 
+one at a time.*/
+
+let name_of_friend = ['sarah', 'ali', 'aroosa', 'laiba'];
+
+for (let index = 0; index < name_of_friend.length; index++) {
+  console.log(name_of_friend[index]);
+}
